4fb6fd2faab455013f478102e915bc9350b7d771
Yohann DONSE <ydonse@e3r4p19.42.fr>
2019-04-25 18:11:16 +0200

    ajout porte + amelioration fonction minimap

--- src/minimap.c
+++ src/minimap.c
@@ -6,11 +6,35 @@
 /*   By: ydonse <ydonse@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/24 16:20:14 by ydonse            #+#    #+#             */
-/*   Updated: 2019/04/25 17:19:32 by malluin          ###   ########.fr       */
+/*   Updated: 2019/04/25 18:09:35 by ydonse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf3d.h"
+
+void	get_case_color(t_main *s, t_position orig, t_position dest, char type)
+{
+	if (type == 'm')
+	{
+		s->sdl->map->color_tmp = 0x9a5444FF;
+		draw_rect(s->sdl, s->sdl->map, orig, dest);
+	}
+	else if (type == '.')
+	{
+		s->sdl->map->color_tmp = 0xB0B0B0FF;
+		draw_rect(s->sdl, s->sdl->map, orig, dest);
+	}
+	else if (type == 'j')
+	{
+		s->sdl->map->color_tmp = 0xA0A0A0FF;
+		draw_rect(s->sdl, s->sdl->map, orig, dest);
+	}
+	else if (type == 'p')
+	{
+		s->sdl->map->color_tmp = 0xCD7006FF;
+		draw_rect(s->sdl, s->sdl->map, orig, dest);
+	}
+}
 
 void	draw_minimap(t_main *s)
 {
@@ -19,9 +43,9 @@ void	draw_minimap(t_main *s)
 	int i;
 	int j;
 
-	i = 0;
+	i = -1;
 	j = 0;
-	while (i < s->height)
+	while (++i < s->height)
 	{
 		while (j < s->width)
 		{
@@ -29,25 +53,8 @@ void	draw_minimap(t_main *s)
 			orig.y = SPACE * i + s->sdl->y_o;
 			dest.x = orig.x + SPACE;
 			dest.y = orig.y + SPACE;
-			if (s->map[i][j].type == 'm')
-			{
-				s->sdl->map->color_tmp = 0x9a5444FF;
-				draw_rect(s->sdl, s->sdl->map, orig, dest);
-			}
-			else if (s->map[i][j].type == '.')
-			{
-				s->sdl->map->color_tmp = 0xB0B0B0FF;
-				draw_rect(s->sdl, s->sdl->map, orig, dest);
-			}
-			else if (s->map[i][j].type == 'j')
-			{
-				s->sdl->map->color_tmp = 0xA0A0A0FF;
-				draw_rect(s->sdl, s->sdl->map, orig, dest);
-			}
-			j++;
+			get_case_color(s, orig, dest, s->map[i][j++].type);
 		}
-		// ft_putchar('\n');
 		j = 0;
-		i++;
 	}
 }
