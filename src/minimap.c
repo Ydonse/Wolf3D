2d2b4474b30cc0aa49770f09a8b5c2a302c946a9
Maxence ALLUIN <malluin@e3r4p17.42.fr>
2019-04-26 11:15:14 +0200

    Merge branch 'master' of https://github.com/Ydonse/Wolf3D

--- src/minimap.c
+++ src/minimap.c
@@ -7,10 +7,35 @@
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/24 16:20:14 by ydonse            #+#    #+#             */
 /*   Updated: 2019/04/25 17:19:32 by malluin          ###   ########.fr       */
+/*   Updated: 2019/04/25 18:09:35 by ydonse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wolf3d.h"
+
+void	get_case_color(t_main *s, t_position orig, t_position dest, char type)
+{
+	if (type == 'm')
+	{
+		s->sdl->map->color_tmp = 0x9a5444FF;
+		draw_rect(s->sdl, s->sdl->map, orig, dest);
+	}
+	else if (type == '.')
+	{
+		s->sdl->map->color_tmp = 0xB0B0B0FF;
+		draw_rect(s->sdl, s->sdl->map, orig, dest);
+	}
+	else if (type == 'j')
+	{
+		s->sdl->map->color_tmp = 0xA0A0A0FF;
+		draw_rect(s->sdl, s->sdl->map, orig, dest);
+	}
+	else if (type == 'p')
+	{
+		s->sdl->map->color_tmp = 0xCD7006FF;
+		draw_rect(s->sdl, s->sdl->map, orig, dest);
+	}
+}
 
 void	draw_minimap(t_main *s)
 {
@@ -20,8 +45,10 @@ void	draw_minimap(t_main *s)
 	int j;
 
 	i = 0;
+	i = -1;
 	j = 0;
 	while (i < s->height)
+	while (++i < s->height)
 	{
 		while (j < s->width)
 		{
@@ -45,6 +72,7 @@ void	draw_minimap(t_main *s)
 				draw_rect(s->sdl, s->sdl->map, orig, dest);
 			}
 			j++;
+			get_case_color(s, orig, dest, s->map[i][j++].type);
 		}
 		// ft_putchar('\n');
 		j = 0;
