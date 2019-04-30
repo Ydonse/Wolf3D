16e2189bc88827617d1cd4853972d36b9c37aa53
Maxence ALLUIN <malluin@e2r4p17.42.fr>
2019-04-30 14:30:53 +0200

    raycast reversed

--- src/controls.c
+++ src/controls.c
@@ -6,7 +6,7 @@
 /*   By: ydonse <ydonse@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/29 09:44:06 by ydonse            #+#    #+#             */
-/*   Updated: 2019/04/30 14:08:24 by ydonse           ###   ########.fr       */
+/*   Updated: 2019/04/30 14:29:36 by malluin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,16 @@
 
 int		keyboard_controls(t_main *s, int key)
 {
-	t_position orig = {0,0};
-	t_position dest = {WIDTH,HEIGHT};
+	// t_position orig = {0,0};
+	// t_position dest = {WIDTH,HEIGHT};
 
-	if (s->sdl->event.type == SDL_KEYDOWN)
-	{
+
 		if (key == SDLK_ESCAPE)
 			return (0);
 		else if (key == SDLK_e)
 			open_door(s);
 		else if (key == SDLK_m)
 			s->active_map = !s->active_map;
-	}
 	return (1);
 }
 
@@ -80,7 +78,7 @@ void	raycast_visualization(t_main *s)
 	int		i;
 
 	i = 0;
-	angle = s->p_angle - s->fov / 2.0;
+	angle = s->p_angle + s->fov / 2.0;
 	// printf("angle debut: %f\n", angle);
 	double correc;
 	correc = raycast(s, s->p_angle);
@@ -91,7 +89,7 @@ void	raycast_visualization(t_main *s)
 			printf("FAIL angle:%f val:%f\n", angle, dist);
 		dist *= cos(to_rad((double)s->p_angle - angle));
 		draw_wall_slice(s, dist, i);
-		angle += s->fov / (double)PROJ_WIDTH;
+		angle -= s->fov / (double)PROJ_WIDTH;
 		i++;
 	}
 	if (s->active_map)
@@ -103,13 +101,18 @@ void	event_handler(t_main *s)
 {
 	const Uint8 *keys;
 
+	keys = SDL_GetKeyboardState(NULL);
 	while (SDL_WaitEvent(&(s->sdl->event)))
 	{
-		keys = SDL_GetKeyboardState(NULL);
+		if (s->sdl->event.type == SDL_KEYDOWN || s->sdl->event.type == SDL_KEYUP)
+		{
+			keys = SDL_GetKeyboardState(NULL);
+			if (s->sdl->event.type == SDL_KEYDOWN)
+				if (keyboard_controls(s, s->sdl->event.key.keysym.sym) == 0)
+					break;
+		}
 		if (s->sdl->event.type == SDL_QUIT)
 			break ;
-		if (keyboard_controls(s, s->sdl->event.key.keysym.sym) == 0)
-			break;
 		if (keys[LEFT] || keys[RIGHT] || keys[UP] || keys[DOWN])
 		{
 			move_player(s,
@@ -118,12 +121,12 @@ void	event_handler(t_main *s)
 		}
 		if (keys[LEFT_AR] || keys[RIGHT_AR])
 		{
-			s->p_angle = (s->p_angle - (keys[LEFT_AR] - keys[RIGHT_AR]) * ROTATE_SPEED + 360) % 360;
+			s->p_angle = (s->p_angle + (keys[LEFT_AR] - keys[RIGHT_AR]) * ROTATE_SPEED + 360) % 360;
 			// printf("%d\n", s->p_angle);
 		}
 		if (keys[UP_AR] || keys[DOWN_AR])
 		{
-			s->viewline = (s->viewline - (keys[UP_AR] - keys[DOWN_AR]) * ROTATE_SPEED);
+			s->viewline = (s->viewline + (keys[UP_AR] - keys[DOWN_AR]) * ROTATE_SPEED);
 			// printf("%d\n", s->p_angle);
 		}
 		if (s->active_map)
