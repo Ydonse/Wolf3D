5437a647436b9ecfc9af790bd21b3f78f2368f25
Maxence ALLUIN <malluin@e1r5p17.42.fr>
2019-05-07 19:08:17 +0200

    opti suite

--- src/raycasting_setup.c
+++ src/raycasting_setup.c
@@ -6,7 +6,7 @@
 /*   By: malluin <malluin@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/30 17:12:40 by malluin           #+#    #+#             */
-/*   Updated: 2019/05/07 18:38:38 by malluin          ###   ########.fr       */
+/*   Updated: 2019/05/07 19:02:00 by malluin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,9 @@ Uint32	darken_color(Uint32 color, double perc)
 	r = color >> 24 & 0xFF;
 	g = color >> 16 & 0xFF;
 	b = color >> 8 & 0xFF;
-	r = (r * (perc));
-	g = (g * (perc));
-	b = (b * (perc));
+	r *= perc;
+	g *= perc;
+	b *= perc;
 	color = (r << 24) | (g << 16) | (b << 8);
 	return (color);
 }
@@ -43,11 +43,11 @@ void	draw_tex_slice(t_main *s, t_ray ray, t_slice sl, double dist)
 	else if (ray.orientation == 'E' || ray.orientation == 'W')
 		perx = (ray.dpos.y - (int)ray.dpos.y);
 	else
-		return;
+		return ;
 	dist = 1 - (dist * 0.125);
 	diff = sl.ewall - sl.bwall;
 	pery = 0;
-	while (sl.pix.y < HEIGHT)
+	while (sl.pix.y < HEIGHT - s->interface->h)
 	{
 		if (pery == 0 || (int)((double)(sl.pix.y - sl.bwall) / diff * s->door->h) > (int)(pery * s->door->h))
 		{
@@ -55,7 +55,6 @@ void	draw_tex_slice(t_main *s, t_ray ray, t_slice sl, double dist)
 			px_tex = (int)(pery * s->door->h) * s->door->w + (int)(perx * s->door->w);
 			if (px_tex >= 0 && px_tex < sl.tex->h * sl.tex->w)
 				sl.color = sl.tex->tex[px_tex];
-			// printf("%08X %08X %08X\n", sl.color, darken_color(sl.color, dist), sl.color - darken_color(sl.color, dist));
 		}
 		set_pixel(s->sdl->game, darken_color(sl.color, dist), sl.pix);
 		sl.pix.y++;
@@ -86,7 +85,8 @@ void	draw_wall_slice(t_main *s, t_ray ray, double dist, int x)
 
 	dist = dist <= 0.01 ? 0.01 : dist;
 	projected_h = ((double)s->proj_distance / dist) / 2.0;
-	dist = dist > 6 ? dist - (dist - 6) * 0.5 : dist;
+	dist = dist > 14 ? 14 : dist;
+	dist = dist > 6 ? dist - (dist - 6) * 0.6 : dist;
 	sl.bwall = s->viewline - projected_h;
 	sl.ewall = s->viewline + projected_h;
 	sl.pix.x = x;
@@ -118,7 +118,7 @@ void	draw_ground_sky(t_main *s, int x)
 	while (++pix.y < s->viewline)
 		set_pixel(s->sdl->game, SKY, pix);
 	set_pixel(s->sdl->game, GROUND, pix);
-	while (++pix.y < HEIGHT)
+	while (++pix.y < HEIGHT - s->interface->h)
 		set_pixel(s->sdl->game, GROUND, pix);
 
 }
@@ -147,6 +147,7 @@ void	raycast_visualization(t_main *s)
 		angle -= s->fov / (double)PROJ_WIDTH;
 		i++;
 	}
-	// draw_weapon(s);
+	draw_weapon(s);
+
 	// printf("angle fin: %f\n", angle);
 }
